f566c25907bcdc6af534e09671bd5a1f13eff3fd
Enrico <enrico.bonato95@gmail.com>
2022-06-05 23:30:28 +0200

    test Stampa

--- src/components/Monthly/MonthlyPrint.vue
+++ src/components/Monthly/MonthlyPrint.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { defineComponent } from 'vue';
 import * as pdfMake from 'pdfmake/build/pdfmake.js';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts.js';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 </script>
 
 <template>
@@ -11,8 +13,31 @@ import * as pdfMake from 'pdfmake/build/pdfmake.js';
 export default defineComponent({
   props: ['month'],
   data() {
-    return { docDefinition: {} }
-  },
+    return {
+      docDefinition: {
+        content: [
+          { text: 'Spesenabrechnung', style: 'header' },
+          'No styling here, this is a standard paragraph',
+          { text: 'Another text', style: 'anotherStyle' },
+          { text: 'Multiple styles applied', style: ['header', 'anotherStyle'] }
+        ],
+      styles: {
+          header: {
+            fontSize: 22,
+            bold: true
+          },
+          anotherStyle: {
+            italics: true,
+            alignment: 'right'
+          }
+        }
+      }
+
+
+    }
+  }
+
+  ,
   methods: {
     print() {
       pdfMake.createPdf(this.docDefinition).download();
