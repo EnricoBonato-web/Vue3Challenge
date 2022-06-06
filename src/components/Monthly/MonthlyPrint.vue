66f3951f91b3829bc6d92e4799952342f9fb6e00
Enrico <enrico.bonato95@gmail.com>
2022-06-06 03:33:44 +0200

    printing

--- src/components/Monthly/MonthlyPrint.vue
+++ src/components/Monthly/MonthlyPrint.vue
@@ -2,6 +2,9 @@
 import { defineComponent } from 'vue';
 import * as pdfMake from 'pdfmake/build/pdfmake.js';
 import * as pdfFonts from 'pdfmake/build/vfs_fonts.js';
+import LocalStorageVar from '@/type/LocalStorageVar';
+import type TripType from '@/type/TripType';
+import type ExpenseType from '@/type/ExpenseType';
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 </script>
 
@@ -10,6 +13,7 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
 </template>
 
 <script lang="ts">
+
 export default defineComponent({
   props: ['month'],
   data() {
@@ -17,30 +21,84 @@ export default defineComponent({
       docDefinition: {
         content: [
           { text: 'Spesenabrechnung', style: 'header' },
-          'No styling here, this is a standard paragraph',
-          { text: 'Another text', style: 'anotherStyle' },
-          { text: 'Multiple styles applied', style: ['header', 'anotherStyle'] }
+          {
+            columns: [
+              {
+                width: 'auto',
+                stack: ['Mitarbeiter', 'Monat']
+              },
+              {
+                width: 'auto',
+                stack: ['Demo User', this.$props.month]
+              }]
+          },
+          { text: 'Reisen', style: 'header' },
+          {
+            columns: []
+          },
+          { text: 'Belege', style: 'header' },
+          {
+            columns: []
+          },
+          { text: 'Spesenabrechnung', style: 'header' },
+          {
+            columns: [
+              {
+                stack: ['Unterschrift des Mitarbeiters:', 'Datum ' + new Date().getDay() + '.' + new Date().getMonth() + '.' + new Date().getFullYear()]
+              },
+              {
+                stack: ['Genehmigt:', 'Datum:']
+              }],
+            style: {
+              pageBreak: 'after'
+            }
+          },
+          { text: 'Spesenabrechnung', pageBreak: 'before' },
+
+
         ],
-      styles: {
+        styles: {
           header: {
-            fontSize: 22,
+            fontSize: 18,
             bold: true
           },
-          anotherStyle: {
-            italics: true,
-            alignment: 'right'
-          }
+          bigger: {
+            fontSize: 15,
+            italics: true
+          },
+        },
+        defaultStyle: {
+          columnGap: 20
         }
       }
-
-
     }
-  }
-
-  ,
+  },
   methods: {
     print() {
-      pdfMake.createPdf(this.docDefinition).download();
+      if (localStorage.getItem(LocalStorageVar.EXPENSES) != null && localStorage.getItem(LocalStorageVar.TRIPS) != null) {
+        const startDate = new Date(Date.parse(this.$props.month + '-01'));
+        const endDate = new Date(Date.parse(this.$props.month + '-01'));
+        endDate.setMonth(endDate.getMonth() + 1)
+
+        let trips: TripType[];
+        trips = JSON.parse(localStorage.getItem(LocalStorageVar.TRIPS)!);
+        trips = trips.filter((trip: TripType) => { return trip.startTime < endDate && trip.endTime >= startDate });
+
+        let expenses: ExpenseType[];
+        expenses = JSON.parse(localStorage.getItem(LocalStorageVar.EXPENSES)!);
+        expenses = expenses.filter((expense: ExpenseType) => { return expense.date < endDate && expense.date >= startDate });
+        //first page render
+        this.docDefinition.content.push({ columns: [{ width: "auto", stack: ['', ''] }] });
+        trips.map((trip: TripType) => {
+
+
+
+
+        })
+
+
+        pdfMake.createPdf(this.docDefinition).download();
+      }
     }
   }
 });
