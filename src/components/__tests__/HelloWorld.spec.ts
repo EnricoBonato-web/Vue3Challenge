1c59e7246e024bfc23d879518853d38279cdd6b2
Enrico <enrico.bonato95@gmail.com>
2022-06-07 04:05:30 +0200

    cypress

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,13 +1,37 @@
-import { mount } from "@cypress/vue";
-import HelloWorld from "../HelloWorld.vue";
+import { mount } from '@cypress/vue';
+import TheWelcome from '../TheWelcome.vue';
 
-describe("HelloWorld", () => {
-  it("playground", () => {
-    mount(HelloWorld, { props: { msg: "Hello Cypress" } });
+describe('HelloWorld', () => {
+  it('playground', () => {
+    mount(TheWelcome, {});
   });
 
-  it("renders properly", () => {
-    mount(HelloWorld, { props: { msg: "Hello Cypress" } });
-    cy.get("h1").should("contain", "Hello Cypress");
+  it('has h3', () => {
+    mount(TheWelcome, { props: { msg: 'Hello Cypress' } });
+    cy.get('h3').should('contain', 'Documentation');
+  });
+  it('renders 1 h3', () => {
+    mount(TheWelcome, { props: { msg: 'Hello Cypress' } });
+    cy.get(':nth-child(1) > .details > h3').should('exist');
+  });
+  it('renders 2 h3', () => {
+    mount(TheWelcome, { props: { msg: 'Hello Cypress' } });
+    cy.get(':nth-child(2) > .details > h3').should('exist');
+  });
+  it('renders 3 h3', () => {
+    mount(TheWelcome, { props: { msg: 'Hello Cypress' } });
+    cy.get(':nth-child(3) > .details > h3').should('exist');
+  });
+  it('renders 4 h3', () => {
+    mount(TheWelcome, { props: { msg: 'Hello Cypress' } });
+    cy.get(':nth-child(4) > .details > h3').should('exist');
+  });
+  it('renders 5 h3', () => {
+    mount(TheWelcome, { props: { msg: 'Hello Cypress' } });
+    cy.get(':nth-child(5) > .details > h3').should('exist');
+  });
+  it('renders 5 h3', () => {
+    mount(TheWelcome, { props: { msg: 'Hello Cypress' } });
+    cy.get(':nth-child(6) > .details > h3').should('not.exist');
   });
 });
